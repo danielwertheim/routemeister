64453a7518b2642c3bfb0c1e194cab156136c01c
Daniel Wertheim <daniel@wertheim.se>
2016-12-07 18:23:36 +0100

    Add EmptyAsyncRouter

---
+++ src/Routemeister/Routers/EmptyAsyncRouter.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace Routemeister.Routers
+{
+    public class EmptyAsyncRouter : IAsyncRouter
+    {
+        public Task RouteAsync<T>(T message)
+        {
+            return Task.FromResult(0);
+        }
+    }
+}
