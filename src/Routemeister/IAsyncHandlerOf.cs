859b15d1db0192e4b38aa51f93bf0b900f65b52a
Daniel Wertheim <daniel@wertheim.se>
2016-12-07 22:26:15 +0100

    Add premade message handler definition for optional use

---
+++ src/Routemeister/IAsyncHandlerOf.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace Routemeister
+{
+    /// <summary>
+    /// One definition of a handler. Please note that you can define your own.
+    /// </summary>
+    /// <typeparam name="TMessage"></typeparam>
+    public interface IAsyncHandlerOf<in TMessage> where TMessage : class
+    {
+        Task HandleAsync(TMessage message);
+    }
+}
