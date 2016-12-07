5fbb0f31ad00c925392f8c50f2041cf75be981a6
Daniel Wertheim <daniel@wertheim.se>
2016-12-07 22:51:00 +0100

    IAsyncMessageHandlerOf

--- src/Routemeister/IAsyncMessageHandlerOf.cs
+++ src/Routemeister/IAsyncMessageHandlerOf.cs
@@ -6,7 +6,7 @@ namespace Routemeister
     /// One definition of a handler. Please note that you can define your own.
     /// </summary>
     /// <typeparam name="TMessage"></typeparam>
-    public interface IAsyncHandlerOf<in TMessage> where TMessage : class
+    public interface IAsyncMessageHandlerOf<in TMessage> where TMessage : class
     {
         Task HandleAsync(TMessage message);
     }
